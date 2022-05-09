f6c8a3161fbb6811d659d9bfd8fac2d091599bde
Yegor <yegor.sidenko@gmail.com>
2022-05-09 15:54:06 +0300

    asdf

---
+++ modules/initbuild/vars.tf
@@ -0,0 +1,8 @@
+variable "aws_region" { }
+variable "aws_profile" { }
+variable "environment" { }
+variable "app_name" { }
+variable "bucket_name" { }
+variable "working_dir" { }
+variable "image_tag" { }
+
